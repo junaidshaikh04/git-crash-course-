41f20e015c363a4ce3f0527d9ba63151142d466c
junaidhsaikh04 <shaikhja24@gmail.com>
2024-02-27 12:45:36 +0530

    code changes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 <head>  
 <script type="text/javascript">  
 function msg(){  
- alert("Hello Javatpoint");  
+ alert("Hello Javascript");  
 }  
 </script>  
 </head>  
 <body>  
-<p>Welcome to JavaScript</p>  
+<p>Welcome to Javatpoint</p>  
 <form>  
 <input type="button" value="click" onclick="msg()"/>  
 </form>  
 </body>  
-</html>  
+</html> 

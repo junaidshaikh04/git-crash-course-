b1a0991c58117940591f46b901f859fdbffabfa5
Shaikh Junaid <116312328+junaidshaikh04@users.noreply.github.com>
2024-02-29 11:47:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
 function msg(){  
  alert("Hello Javascript");  
 }  
+ <p>Welcome to Javatpoint</p>  
+<form>  
+<input type="button" value="click" onclick="msg()"/>  
+</form>  
+</body>  
+</html> 
+ 
 </script>  
 </head>  
 <body>  
